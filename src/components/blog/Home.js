8cc1419022d021345b443d907ca509658948b4db
unknown <kumar.ankit383@gmail.com>
2021-02-21 07:10:52 +0530

    Blog card added

--- src/components/blog/Home.js
+++ src/components/blog/Home.js
@@ -5,10 +5,11 @@ import ReactMarkdownWithHtml  from "react-markdown";
 import gfm from 'remark-gfm';
 import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
 import {materialLight} from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { Link } from 'react-router-dom';
 
 const renderers = {
     code: ({language, value}) => {
-      return <SyntaxHighlighter style={materialLight} language={language} children={value} />
+      return <SyntaxHighlighter style={materialLight} language={language?language:'js'} children={value} />
     }
 }
 
@@ -26,26 +27,49 @@ class Home extends Component {
         fetch('http://127.0.0.1:8000/api/blog/all/')
         .then(res => res.json())
         .then(data => {
-            this.setState({blogs : data, loading:false})
+            setTimeout(() => {
+                this.setState({blogs : data, loading:false})
+            }, 500);
         })
         .catch(err => console.log(err))
     }
 
     render () {
 
+        let blogs = null;
+        if (this.state.blogs.length>0){
+            blogs = this.state.blogs.map(blog => {
+                return (
+                    <div className="col-sm-4">
+                        <div className="card">
+                            <div className="card-body">
+                                
+                                {blog.cover_image ? <div style={{height:"200px", width:"100%", background:`url(http://127.0.0.1:8000${blog.cover_image}/)`, backgroundPosition:"center", backgroundSize : "cover"}}>
+
+                                </div> : null}
+
+                                <h3 style={{marginTop:"10px"}}> <Link to={`/blog/${blog.slug}`}>{blog.title}</Link> </h3>
+
+                                <p>{blog.description}</p>
+                                <p>{new Date(blog.published_on).toDateString()}</p>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })
+        }
+
         return (
             <div className="container">
-                <h3>Featured blogs</h3>
                 {
                     /** When no blogs found */
                     !this.state.loading && this.state.blogs.length <= 0 ? <p style={{textAlign:"center"}}>No blogs found</p> : null
                 }
                 {
-                    this.state.loading ? <Loader /> : null
+                    this.state.loading ? <Loader /> : <div className="row">
+                        {blogs}
+                        </div>
                 }
-                <ReactMarkdownWithHtml plugins={[gfm]} renderers={renderers} escapeHtml={false} >
-                    {this.state.blogs[0] ? this.state.blogs[0].body : null}
-                </ReactMarkdownWithHtml>
             </div>
         )
     }
